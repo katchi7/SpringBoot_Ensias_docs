9d6aca62058983647bc14bfb6e02361368768e4b
bouthaina-hrour <73646814+bouthaina-hrour@users.noreply.github.com>
2021-06-04 12:52:10 +0200

    first repo

---
+++ src/main/java/com/ensias/Ensias_docs/repositpries/DocumentRepository.java
@@ -0,0 +1,12 @@
+package com.ensias.Ensias_docs.repositpries;
+
+import com.ensias.Ensias_docs.models.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document,Long> {
+
+    
+
+}

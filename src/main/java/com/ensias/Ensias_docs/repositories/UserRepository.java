ccb764773c57a3c5e4c241eb52f601e78f0e0fb0
fatimazahra-azennag <58732724+fatimazahra-azennag@users.noreply.github.com>
2021-06-04 12:58:18 +0200

    user + todos Repository

---
+++ src/main/java/com/ensias/Ensias_docs/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ensias.Ensias_docs.repositories;
+
+import com.ensias.Ensias_docs.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
